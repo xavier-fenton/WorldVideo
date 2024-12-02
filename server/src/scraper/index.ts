fac88e23001bdf74dfd728a25f1b007299435c3d
Xavier Fenton <xavierfenton99@gmail.com>
2024-12-02 13:27:04 +1300

    organize directories & dont push null vals

--- server/src/scraper/index.ts
+++ server/src/scraper/index.ts
@@ -1,13 +1,42 @@
-import axios from "axios";
-import * as cheerio from "cheerio";
-
-// const response = await axios.get("https://web-scraping.dev/product/1");
-const response = await axios.get("https://youtube.com");
+import { chromium } from 'playwright';
 
 
-const selector = cheerio.load(response.data);
+async function main() {
+    // Launch a new instance of a Chromium browser with headless mode
+    // disabled for visibility
+    const browser = await chromium.launch({
+        headless: true
+    });
 
-console.log({
-    "h3": selector("h3").first().text(),    
-});
+    // Create a new Playwright context to isolate browsing session
+    const context = await browser.newContext();
+    // Open a new page/tab within the context
+    const page = await context.newPage();
 
+    // Navigate to the youtube feed 
+    await page.goto('https://www.youtube.com/feed/trending');
+
+    let storeLinks: Array<string | null> = [];    
+
+    const locateVideos = page.locator("ytd-thumbnail.style-scope").all()
+
+    const extractedVideos = (await locateVideos).map(async (thumbnail) => 
+    {
+        const info = await thumbnail.locator('a#thumbnail').getAttribute('href')
+        if(info !== null)
+        {
+            storeLinks.push(info)
+        }
+    })
+
+    await page.waitForTimeout(5000);
+    
+    console.log(storeLinks);
+
+
+    // Close the browser instance after task completion
+    await browser.close();
+    }
+
+// Execute the main function
+main();
