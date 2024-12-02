1add38ecbdb9512fa81813c8ac52664d0e56e1c7
Xavier Fenton <xavierfenton99@gmail.com>
2024-12-03 11:55:41 +1300

    Connect to mongoDB

---
+++ server/src/db.ts
@@ -0,0 +1,35 @@
+
+import { MongoClient, ServerApiVersion } from "mongodb"
+import dotenv from 'dotenv'
+
+const envConfig = dotenv.config()
+
+if (envConfig.error) {
+  throw envConfig.error
+}
+
+
+const uri = process.env.MONGO_DB_URI as string;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
