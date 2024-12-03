172fe8afd0e05e9d18a4de8b02db229b82023344
Xavier Fenton <xavierfenton99@gmail.com>
2024-12-04 11:29:03 +1300

    Server: Add headers to accept dev server

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,24 @@ import ScraperV1 from './scraper/index'
 
 export const server = express();
 
+// Add headers before the routes are defined
+server.use((req, res, next) => {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  // res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  next();
+});
+
 server.use('/v1/scrape', async (req, res) => {
     
     
