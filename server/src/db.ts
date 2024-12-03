bd83b7fd9b2b50931b6d1f4e6591fbca1f60080f
Xavier Fenton <xavierfenton99@gmail.com>
2024-12-03 14:33:40 +1300

    Set up MongoDB (doesn't work)

--- server/src/db.ts
+++ server/src/db.ts
@@ -8,28 +8,42 @@ if (envConfig.error) {
   throw envConfig.error
 }
 
+export const db = async () => {
+    const uri = process.env.MONGO_DB_URI as string;
 
-const uri = process.env.MONGO_DB_URI as string;
+    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+    const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+    });
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+    const db = client.db("sample_mflix")
+    const collection = db.collection("movies")
+    
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+    return {
+        getTest: async () => {
+            try {
+                
+               const result = collection.find({})
+               return result
+            } catch (error) {
+               
+                    console.error(error)
+                    console.trace()
+                }
+            }
+            
+        }
+    }
+
+
+
+
+
+
+
+
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,12 @@
 import express from "express"
 import ScraperV1 from './scraper/index'
+import { router } from "./router"
 
 
-export const server = express()
+export const server = express();
 
-server.use('/v1/api', async (req, res) => {
-    try {
-      const response = await ScraperV1();
-      const data = await JSON.parse(response)
-      res.json(data);
-    } catch (error) {
-      res.status(500).json({ error: 'Something went wrong!' });
-    }
-})
+server.use('/', router);
 
 server.get('/', (req, res) => {
     res.sendStatus(200)
-})
+});
